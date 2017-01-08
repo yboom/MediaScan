60e942d602676f37b76b98a9f94617b449cfc1c3
luweiewul <yboom@icloud.com>
2017-01-08 17:49:48 +0200

    working basically

---
+++ app/src/main/java/com/yboom/mediascan/ApplicationLoader.java
@@ -0,0 +1,16 @@
+package com.yboom.mediascan;
+
+import android.app.Application;
+import android.content.Context;
+
+public class ApplicationLoader extends Application{
+   
+	 public static volatile Context applicationContext = null;
+	
+	
+	@Override
+    public void onCreate() {
+        super.onCreate();
+        applicationContext = getApplicationContext();
+    }
+}
---
+++ app/src/main/java/com/yboom/mediascan/MainActivity.java
@@ -0,0 +1,85 @@
+package com.yboom.mediascan;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private FragmentManager fragmentManager = null;
+    private FragmentTransaction fragmentTransaction = null;
+    private DirectoryFragment mDirectoryFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        toolbar.setTitle("Directory");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        mDirectoryFragment = new DirectoryFragment();
+        mDirectoryFragment.setDelegate(new DirectoryFragment.DocumentSelectActivityDelegate() {
+
+            @Override
+            public void startDocumentSelectActivity() {
+
+            }
+
+            @Override
+            public void didSelectFiles(DirectoryFragment activity,
+                                       ArrayList<String> files) {
+                mDirectoryFragment.showErrorBox(files.get(0).toString());
+            }
+
+            @Override
+            public void updateToolBarName(String name) {
+                toolbar.setTitle(name);
+
+            }
+        });
+        fragmentTransaction.add(R.id.fragment_container, mDirectoryFragment, "" + mDirectoryFragment.toString());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    @Override
+    protected void onDestroy() {
+        mDirectoryFragment.onFragmentDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDirectoryFragment.onBackPressed_()) {
+            super.onBackPressed();
+        }
+    }
+}
